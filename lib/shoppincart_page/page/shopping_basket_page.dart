b486f007bcfcbee4d7016ea833205c09c9e38e48
whwlgns42 <whwlgns42@kakao.com>
2022-11-21 20:14:54 +0900

    장바구니 프론트부분 작업 완료 기능 구현 필요

--- lib/shoppincart_page/page/shopping_basket_page.dart
+++ lib/shoppincart_page/page/shopping_basket_page.dart
@@ -52,13 +52,13 @@ class _ShoppingBasketPageState extends State<ShoppingBasketPage> {
               ),
               Column(
                 children: [
-                  _lineBar(), // 구분선
+                  _buildLineBar(), // 구분선
                   Padding(
                     padding: const EdgeInsets.only(top: 30, bottom: 10),
                     child: Text(
                       "장바구니에 담긴 상품이 없습니다.",
                       style:
-                      TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                     ),
                   ),
                   Padding(
@@ -68,11 +68,16 @@ class _ShoppingBasketPageState extends State<ShoppingBasketPage> {
                   _buildShoppingCartImage(), // 쇼핑카트 이미지
                   SizedBox(height: 20),
                   buildLoginButton(), // 로그인 버튼
-                  _guide(),
-                  _lineBar(),
+                  _buildGuide(),
+                  _buildLineBar(),
+                  _buildItemTitle("굿즈"),
+                  _buildItemList(),
+                  _buildLineBar(),
+                  _buildItemTitle("커피"),
+                  _buildCoffeeList(),
+
                   SizedBox(height: 50),
                   Footer(),
-
                 ],
               )
             ],
@@ -122,32 +127,48 @@ class _ShoppingBasketPageState extends State<ShoppingBasketPage> {
         onPressed: () {
           Navigator.pushNamed(context, "/login");
         },
-        child: Text("로그인하기", style: TextStyle(
-          color: Colors.black54,
-        ),),
+        child: Text(
+          "로그인하기",
+          style: TextStyle(
+            color: Colors.black54,
+          ),
+        ),
       ),
     );
   }
 
-  _guide() {
+  _buildGuide() {
     return Container(
       child: Column(
         children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextButton(onPressed: () {}, child: Text("무이자 할부 안내", style: TextStyle(
-                color: Colors.black54,
-              ),)),
+              TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "무이자 할부 안내",
+                    style: TextStyle(
+                      color: Colors.black54,
+                    ),
+                  )),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 5),
-                child: Text("|", style: TextStyle(
-                  color: Colors.black54,
-                ),),
+                child: Text(
+                  "|",
+                  style: TextStyle(
+                    color: Colors.black54,
+                  ),
+                ),
               ),
-              TextButton(onPressed: () {}, child: Text("장바구니 이용안내", style: TextStyle(
-                color: Colors.black54,
-              ),)),
+              TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "장바구니 이용안내",
+                    style: TextStyle(
+                      color: Colors.black54,
+                    ),
+                  )),
             ],
           ),
           SizedBox(height: 30),
@@ -156,11 +177,200 @@ class _ShoppingBasketPageState extends State<ShoppingBasketPage> {
     );
   }
 
-  _lineBar() {
+  _buildLineBar() {
     return Container(
       width: double.infinity,
       height: 20,
       color: Color.fromRGBO(219, 218, 222, 1),
+    );
+  }
+
+  //
+  _buildItemTitle(String itemTitleName) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 8, top: 10, right: 5, bottom: 10),
+      child: Container(
+        child: Row(
+          children: [
+            Text(
+              "알라딘 ",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                  fontSize: 22),
+            ),
+            Text(
+              "$itemTitleName",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.pink,
+                fontSize: 22,
+              ),
+            ),
+            Spacer(),
+            TextButton(
+                onPressed: () {},
+                child: Text(
+                  "더보기 > ",
+                  style: TextStyle(color: Colors.grey),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildItemImg(String itemsImgUrl, String itemName) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  offset: Offset(0.0, 1.0), //(x,y)
+                  blurRadius: 6.0,
+                ),
+              ],
+            ),
+            child: Image(
+              image: AssetImage("assets/${itemsImgUrl}.jpg"),
+              width: 150,
+              height: 150,
+              fit: BoxFit.fitHeight,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          SizedBox(
+            width: 150,
+            child: Text(
+              itemName,
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  _buildItemList() {
+    return Container(
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children: [
+            InkWell(
+                onTap: () {},
+                child: _buildItemImg("goodsItem1", "본투리드 베이직 노트")),
+            InkWell(
+                onTap: () {}, child: _buildItemImg("goodsItem2", "본투리드 디자인연필")),
+            InkWell(
+                onTap: () {},
+                child: _buildItemImg("goodsItem3", "본투리드 볼펜(유성)")),
+            InkWell(
+                onTap: () {},
+                child: _buildItemImg("goodsItem4", "본투리드 투명 북마크")),
+            InkWell(
+                onTap: () {},
+                child: _buildItemImg("goodsItem5", "문장 부호 스티키 마커")),
+            InkWell(
+                onTap: () {}, child: _buildItemImg("goodsItem6", "본투리드 엘홀더")),
+            InkWell(
+                onTap: () {},
+                child: _buildItemImg("goodsItem7", "스티키 북마크(120매)")),
+            InkWell(
+                onTap: () {},
+                child: _buildItemImg("goodsItem8", "본투리드 실리콘 조리...")),
+            InkWell(
+                onTap: () {},
+                child: _buildItemImg("goodsItem9", "본투리드 초저점도 3")),
+            InkWell(
+                onTap: () {},
+                child: _buildItemImg("goodsItem10", "본투리드 젓가락_우드")),
+            InkWell(
+                onTap: () {},
+                child: _buildItemImg("goodsItem11", "피너츠 카세트 테이프")),
+            InkWell(
+                onTap: () {}, child: _buildItemImg("goodsItem12", "본투리드 지우개")),
+            _buildAddIcon(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // 더보기 버튼
+  _buildAddIcon() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 30, right: 80, bottom: 70),
+      child: Container(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 40, top: 50),
+              child: Container(
+                  child: Icon(
+                Icons.add_circle_outline,
+                size: 100,
+              )),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 20,
+                left: 35,
+              ),
+              child: Text(
+                "더보기",
+                style: TextStyle(fontSize: 30, color: Colors.grey),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _buildCoffeeList() {
+    return Container(
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children: [
+            InkWell(
+                onTap: () {},
+                child: _buildItemImg("coffee1", "콜롬비아 엑셀소 디카...")),
+            InkWell(
+                onTap: () {},
+                child: _buildItemImg("coffee2", "트립백 콜롬비아 엑셀...")),
+            InkWell(
+                onTap: () {},
+                child: _buildItemImg("coffee3", "드립백 브라질 산타 루...")),
+            InkWell(
+                onTap: () {}, child: _buildItemImg("coffee4", "에티오피아 예가체프...")),
+            InkWell(
+                onTap: () {}, child: _buildItemImg("coffee5", "에티오피아 예가체프...")),
+            InkWell(
+                onTap: () {},
+                child: _buildItemImg("coffee6", "콜드브루 파우치 알라...")),
+            InkWell(
+                onTap: () {},
+                child: _buildItemImg("coffee7", "콜드브루 파우치 알라...")),
+            InkWell(
+                onTap: () {}, child: _buildItemImg("coffee8", "브라질 산타 루시아 #5")),
+            InkWell(
+                onTap: () {},
+                child: _buildItemImg("coffee9", "콜드브루 온두라스 엘...")),
+            _buildAddIcon(),
+          ],
+        ),
+      ),
     );
   }
 }
